50e2ae877106ef8c081482d1f0b8ead53ee26ee3
Josh Bleecher Snyder <josharian@gmail.com>
2018-11-22 09:34:08 -0800

    starlark: focus BenchmarkHashTable on the hash table
    BenchmarkHashTable execution time is dominated by Zipf;
    its memory allocation is dominated by MakeInt.
    
    Move all that work up front, so that it doesn't impact the benchmark.
    
    This obviously doesn't actually impact the true performance,
    but to get a sense for how much overhead this removes:
    
    name         old time/op    new time/op    delta
    Hashtable-8    4.59ms ± 1%    1.35ms ± 1%  -70.59%  (p=0.000 n=9+10)
    
    name         old alloc/op   new alloc/op   delta
    Hashtable-8     299kB ± 0%     137kB ± 0%  -54.11%  (p=0.000 n=10+10)
    
    name         old allocs/op  new allocs/op  delta
    Hashtable-8     7.84k ± 0%     0.02k ± 0%  -99.72%  (p=0.000 n=10+10)

--- starlark/hashtable_test.go
+++ starlark/hashtable_test.go
@@ -7,10 +7,12 @@ package starlark
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"testing"
 )
 
 func TestHashtable(t *testing.T) {
+	makeTestIntsOnce.Do(makeTestInts)
 	testHashtable(t, make(map[int]bool))
 }
 
@@ -34,39 +36,64 @@ func BenchmarkStringHash(b *testing.B) {
 }
 
 func BenchmarkHashtable(b *testing.B) {
-	// TODO(adonovan): MakeInt probably dominates the cost of this benchmark.
-	// Optimise or avoid it.
+	makeTestIntsOnce.Do(makeTestInts)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		testHashtable(b, nil)
+	}
+}
+
+const testIters = 10000
+
+var (
+	// testInts is a zipf-distributed array of Ints and corresponding ints.
+	// This removes the cost of generating them on the fly during benchmarking.
+	// Without this, Zipf and MakeInt dominate CPU and memory costs, respectively.
+	makeTestIntsOnce sync.Once
+	testInts         [3 * testIters]struct {
+		Int   Int
+		goInt int
+	}
+)
+
+func makeTestInts() {
+	zipf := rand.NewZipf(rand.New(rand.NewSource(0)), 1.1, 1.0, 1000.0)
+	for i := range &testInts {
+		r := int(zipf.Uint64())
+		testInts[i].goInt = r
+		testInts[i].Int = MakeInt(r)
 	}
 }
 
 // testHashtable is both a test and a benchmark of hashtable.
 // When sane != nil, it acts as a test against the semantics of Go's map.
 func testHashtable(tb testing.TB, sane map[int]bool) {
-	zipf := rand.NewZipf(rand.New(rand.NewSource(0)), 1.1, 1.0, 1000.0)
+	var i int // index into testInts
+
 	var ht hashtable
 
 	// Insert 10000 random ints into the map.
-	for j := 0; j < 10000; j++ {
-		k := int(zipf.Uint64())
-		if err := ht.insert(MakeInt(k), None); err != nil {
+	for j := 0; j < testIters; j++ {
+		k := testInts[i]
+		i++
+		if err := ht.insert(k.Int, None); err != nil {
 			tb.Fatal(err)
 		}
 		if sane != nil {
-			sane[k] = true
+			sane[k.goInt] = true
 		}
 	}
 
 	// Do 10000 random lookups in the map.
-	for j := 0; j < 10000; j++ {
-		k := int(zipf.Uint64())
-		_, found, err := ht.lookup(MakeInt(k))
+	for j := 0; j < testIters; j++ {
+		k := testInts[i]
+		i++
+		_, found, err := ht.lookup(k.Int)
 		if err != nil {
 			tb.Fatal(err)
 		}
 		if sane != nil {
-			_, found2 := sane[k]
+			_, found2 := sane[k.goInt]
 			if found != found2 {
 				tb.Fatal("sanity check failed")
 			}
@@ -74,18 +101,19 @@ func testHashtable(tb testing.TB, sane map[int]bool) {
 	}
 
 	// Do 10000 random deletes from the map.
-	for j := 0; j < 10000; j++ {
-		k := int(zipf.Uint64())
-		_, found, err := ht.delete(MakeInt(k))
+	for j := 0; j < testIters; j++ {
+		k := testInts[i]
+		i++
+		_, found, err := ht.delete(k.Int)
 		if err != nil {
 			tb.Fatal(err)
 		}
 		if sane != nil {
-			_, found2 := sane[k]
+			_, found2 := sane[k.goInt]
 			if found != found2 {
 				tb.Fatal("sanity check failed")
 			}
-			delete(sane, k)
+			delete(sane, k.goInt)
 		}
 	}
 
